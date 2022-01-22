fd84e36903f661e8e615726f04396ea3abffbacd
KanekiCow <90784392+KanekiCow@users.noreply.github.com>
2022-01-22 14:25:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 
 
-const express = require('express')
-const app = express();
-const port = 3000
-
-app.get('/', (req, res) => res.send('Yo boi!!'))
-
-app.listen(port, () =>
-console.log(`Your app is listening a http://localhost:${port}`)
-);
-
 const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
